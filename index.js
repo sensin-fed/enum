5db7ddf70d7fcf3cc980ed7676b1d4a8dc40646d
刘剑 <liujian@magicare.me>
2019-06-03 16:39:14 +0800

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+export const sex = {
+  0: '未知',
+  1: '男',
+  2: '女',
+};
+
+export const wagesUnit = {
+  1: '天',
+  2: '月',
+  3: '年',
+};
+
+export const dataOrigin = {
+  1: '网站',
+  2: '企业',
+};
+
+export default {};
