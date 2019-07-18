def37d33711f870fe43695f65358821ab5395c55
刘剑 <liujian@magicare.me>
2019-07-18 12:28:09 +0800

    添加pcms

---
+++ pcms.js
@@ -0,0 +1,6 @@
+export const courseState = {
+  1: '未上架',
+  2: '已上架',
+};
+
+export default {};
--- ulms.js
+++ ulms.js
@@ -1,5 +1,7 @@
-const lessonType = {
+export const lessonType = {
   1: '1v1直播',
   2: '1v多直播',
   3: '录播',
 };
+
+export default {};
