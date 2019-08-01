f5d9aa10db19dc31bb13f717686d23f8c2bc44e0
Joie <qinyijuan@offerxiansheng.com>
2019-08-01 16:45:58 +0800

    fix

--- pcms.js
+++ pcms.js
@@ -1,12 +1,18 @@
+/*
+ * @Description: pcms 枚举
+ * @Author: Joie Qin
+ * @Date: 2019-07-22 09:34:51
+ * @LastEditors: Joie Qin
+ * @LastEditTime: 2019-07-31 11:53:07
+ */
 export const courseState = {
   1: '未上架',
   2: '已上架',
 };
 
 export const lessonType = {
-  1: '图文',
-  2: '视频',
-  3: '作业',
+  1: '教学内容',
+  2: '作业',
 };
 
 export const orderState = {
