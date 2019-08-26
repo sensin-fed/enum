000c3ddbf2f565b82282c386abcf900f21df6aee
hemanyuan <hemanyuan@offerxiansheng.com>
2019-08-26 11:44:06 +0800

    新增：官网项目枚举

---
+++ portal.js
@@ -0,0 +1,23 @@
+/*
+ * @Description: 官网项目枚举值
+ * @Author: hemanyuan
+ * @Date: 2019-08-26 11:42:11
+ * @LastEditors: hemanyuan
+ * @LastEditTime: 2019-08-26 11:42:22
+ */
+export const commonRouter = [
+  '/',
+  '/index',
+  '/checkout',
+  '/courseDetail',
+  '/learn',
+  '/myCourse',
+  '/order',
+  '/orderDetail',
+  '/paidResult',
+  '/orderDetail',
+  '/privacyPolicy',
+  '/userSetting',
+  '/orderDetail',
+  '/orderDetail',
+];
