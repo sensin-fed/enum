afdc03306439b2920391b2a2de8212fb7f96b226
hemanyuan <hemanyuan@offerxiansheng.com>
2019-12-25 17:18:27 +0800

    add offer满意度枚举

--- ulms.js
+++ ulms.js
@@ -1,14 +1,22 @@
 export const lessonType = {
   1: '1v1直播',
   2: '1v多直播',
-  3: '录播'
-}
+  3: '录播',
+};
 
 export const newLessonType = {
   1: '1v1课程',
   2: '小班课',
   3: '录播课',
-  4: '群面模拟'
-}
+  4: '群面模拟',
+};
 
-export default {}
+export const offerRate = {
+  1: '非常不满意',
+  2: '不满意',
+  3: '一般',
+  4: '满意',
+  5: '非常满意',
+};
+
+export default {};
