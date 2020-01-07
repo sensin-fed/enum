199f95e69458cf498a46d6e5174b0c39e1090f74
hemanyuan <hemanyuan@offerxiansheng.com>
2020-01-07 11:02:42 +0800

    add fuxi枚举

---
+++ fuxi.js
@@ -0,0 +1,35 @@
+// 投递状态
+export const jobStageStatus = {
+  1: '投递',
+  2: '笔试',
+  3: '面试',
+  4: '获得offer',
+  5: '不匹配',
+};
+
+// 职位类型
+export const jobType = {
+  1: '校招',
+  2: '社招',
+  3: '实习',
+};
+
+// 学员状态
+export const studentStatus = {
+  1: '待安排',
+  2: '进行中',
+  3: '已结课',
+  4: '已休学',
+  5: '已退款',
+};
+
+// offer满意度
+export const offerRate = {
+  1: '非常不满意',
+  2: '不满意',
+  3: '一般',
+  4: '满意',
+  5: '非常满意',
+};
+
+export default {};
