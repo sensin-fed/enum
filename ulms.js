7036b3cb469816e892b7b99817aebd2481c14983
刘剑 <liujian@magicare.me>
2020-01-08 16:07:57 +0800

    加入新枚举

--- ulms.js
+++ ulms.js
@@ -29,8 +29,8 @@ export const stageType = {
 
 export const stage = {
   1: '简历筛选',
-  2: '笔试中',
-  3: '面试中',
+  2: '笔试',
+  3: '面试',
   4: '获得offer',
   5: '不匹配',
 };
@@ -44,6 +44,44 @@ export const offerType = {
 export const interviewType = {
   1: '单面',
   2: '群面',
+};
+
+export const singleInterviewType = {
+  1: 'HR面',
+  2: '经理面',
+  3: '总裁面',
+};
+
+export const groupInterviewType = {
+  1: '辩论',
+  2: '无领导小组讨论',
+  3: '即兴演讲',
+  4: '多V多',
+};
+
+export const whether = {
+  1: '是',
+  2: '否',
+};
+
+export const lang = {
+  1: '中文',
+  2: '英文',
+  3: '中英文均有',
+};
+
+export const positionType = {
+  1: '校招',
+  2: '社招',
+  3: '实习',
+};
+
+export const tier = {
+  0: '0',
+  1: '1',
+  2: '2',
+  3: '3',
+  4: '4',
 };
 
 export default {};
