0f783ef7dd72f8ae4238e30cf78556f616b1e06b
hemanyuan <hemanyuan@offerxiansheng.com>
2020-05-09 14:06:03 +0800

    update ulms format

--- ulms.js
+++ ulms.js
@@ -1,88 +1,88 @@
 export const lessonType = {
   1: '1v1直播',
   2: '1v多直播',
-  3: '录播',
-};
+  3: '录播'
+}
 
 export const newLessonType = {
   1: '1v1课程',
   2: '小班课',
   3: '录播课',
   4: '群面模拟',
-  5: '求职',
-};
+  5: '求职'
+}
 
 export const offerRate = {
   1: '非常不满意',
   2: '不满意',
   3: '一般',
   4: '满意',
-  5: '非常满意',
-};
+  5: '非常满意'
+}
 
 export const stageType = {
   1: '投递',
   2: '笔试',
   3: '面试',
   4: '获得offer',
-  5: '不匹配',
-};
+  5: '不匹配'
+}
 
 export const stage = {
   1: '简历筛选',
   2: '笔试中',
   3: '面试中',
   4: '获得offer',
-  5: '不匹配',
-};
+  5: '不匹配'
+}
 
 export const offerType = {
   1: '待定',
   2: '接受',
-  3: '拒绝',
-};
+  3: '拒绝'
+}
 
 export const interviewType = {
   1: '单面',
-  2: '群面',
-};
+  2: '群面'
+}
 
 export const singleInterviewType = {
   1: 'HR面',
   2: '经理面',
-  3: '总裁面',
-};
+  3: '总裁面'
+}
 
 export const groupInterviewType = {
   1: '辩论',
   2: '无领导小组讨论',
   3: '即兴演讲',
-  4: '多V多',
-};
+  4: '多V多'
+}
 
 export const whether = {
   1: '是',
-  2: '否',
-};
+  2: '否'
+}
 
 export const lang = {
   1: '中文',
   2: '英文',
-  3: '中英文均有',
-};
+  3: '中英文均有'
+}
 
 export const positionType = {
   1: '校招',
   2: '社招',
-  3: '实习',
-};
+  3: '实习'
+}
 
 export const tier = {
   0: '0',
   1: '1',
   2: '2',
   3: '3',
-  4: '4',
-};
+  4: '4'
+}
 
-export default {};
+export default {}
